4cf455caace35bdffbdc4ea3e8e9dfb748b03d51
xjl <353167732@qq.com>
2019-11-15 17:38:23 +0800

    webpack-dev-server配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,34 @@ const path = require("path");
 
 module.exports = {
     // 单入口、单出口
-    // entry: "./public/index.js",
-    // output:{
-    //     path:path.resolve(__dirname,'build'),
-    //     filename:'bundle.js'
-    // }
+    entry: "./public/index.js",
+    output:{
+        path:path.resolve(__dirname,'build'),
+        filename:'bundle.js'
+    },
+
     // 多入口、单出口
     // entry: ["./public/index.js", './public/index2.js'],
     // output:{
     //     path:path.resolve(__dirname,'build'),
     //     filename:'bundle.js'
     // }
+
     // 多入口、多出口
-    entry: {
-        page1:'./public/page1/index.js',
-        page2:'./public/page2/index.js'
-    },
-    output: {
-        path:path.resolve(__dirname,'build'),
-        filename: '[name].js'
+    // entry: {
+    //     page1:'./public/page1/index.js',
+    //     page2:'./public/page2/index.js'
+    // },
+    // output: {
+    //     path:path.resolve(__dirname,'build'),
+    //     filename: '[name].js'
+    // },
+
+    // 本地开服服务器配置
+    devServer: {
+        contentBase: './build', // 服务器访问的目录，一般只想打包完的出口目录
+        host: 'localhost', // 服务器的ip地址
+        port: 8000, // 服务器端口
+        open: true // 运行时自动打开页面
     }
-}
+};
